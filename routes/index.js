6dbdeec2189aeb40d676568c76c09b5712854ede
Mohammed Shahul P M <mshahulpm@gmail.com>
2020-09-26 16:17:37 +0530

    part 1 is competed

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var src = require('./imag')
+let products = [
+  {
+    name: 'paripp',
+    price: 88,
+    img: src
+  },
+  {
+    name: 'cherupayar - 1',
+    price: 128,
+    img: src
+  },
+  {
+    name: 'cherupayar - 2',
+    price: 108,
+    img: src
+  },
+  {
+    name: 'കടല',
+    price: 76,
+    img: src
+  }
+]
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { products, admin: false });
 });
 
 module.exports = router;
